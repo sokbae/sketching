bbca9c1c824f784e872fb14d4f5e25fc2186c243
Simon Lee <sl3841@columbia.edu>
2022-06-06 16:45:10 +0100

    Tests further updated

--- tests/testthat/test_sketching.R
+++ tests/testthat/test_sketching.R
@@ -6,6 +6,14 @@ test_that("m must be strictly smaller than n", {
   
 })
 
+test_that("m must be strictly smaller than n", {
+  
+  fullsample <- matrix(rnorm(100),nrow=20,ncol=5)
+  
+  expect_error(sketch_leverage(fullsample, 20), "Error: m needs to be strictly smaller than n.")
+  
+})
+
 test_that("Different DGPs generate different data", {
   
   data <- simulation_dgp(100, 5, hetero = TRUE)
@@ -21,9 +29,3 @@ test_that("Different DGPs generate different data", {
   expect_false(model1$coefficients[1] == model2$coefficients[1])
   
 })
-
-
-data <- simulation_dgp(100, 5, hetero = TRUE)
-y <- data$Y
-x <- data$X
-model  <- lm(y ~ x) 
