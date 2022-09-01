9a7e7e8d64cb16baa454a2b2c38737cea52f45d2
Simon Lee <sl3841@columbia.edu>
2022-09-01 22:16:12 +0100

    namespace updated

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 rcpp_count_sketch <- function(X, hv, gv, m) {
-    .Call('_sketching_rcpp_count_sketch', PACKAGE = 'sketching', X, hv, gv, m)
+    .Call(`_sketching_rcpp_count_sketch`, X, hv, gv, m)
 }
 
